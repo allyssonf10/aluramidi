24731ba698c3ec02f09cf10032a08087ede2b75b
allyssonf10 <allysson.nevesf10@gmail.com>
2022-10-25 13:10:27 -0300

    teclado funcionando

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < ListaDeTeclas.length){
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    };
+    contador = contador + 1;
+
+    //console.log(contador);
+}
