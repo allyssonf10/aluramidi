0c7dbca6a4bae7426609ba87b2360e1c18ff78bc
allyssonf10 <allysson.nevesf10@gmail.com>
2022-10-31 23:24:35 -0300

    tecla space

--- main.js
+++ main.js
@@ -23,13 +23,13 @@ for (let contador=0; contador < ListaDeTeclas.length; contador++){
 
     tecla.onkeydown = function (evento) {
 
-    console.log(evento.code == 'Enter');
-
-        if(evento.code == 'Enter' ){
+        if(evento.code === 'Enter' ){
             tecla.classList.add('ativa'); 
         }             
         
-        
+        if(evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function(){
